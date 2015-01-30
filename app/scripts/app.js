1477934a272deca8f5ff61406f46b019b74fa37c
Sergio Toro <yiti007@gmail.com>
2015-01-30 16:55:44 +0100

    Map page, include map directive.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,24 +9,39 @@
  * Main module of the application.
  */
 angular
-  .module('foundersMapQuestApp', [
-    'ngAnimate',
-    'ngMessages',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('foundersMapQuestApp', [
+        'ngAnimate',
+        'ngMessages',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'ngMap',
+        'LocalStorageModule',
+    ])
+    .config(function ($routeProvider, localStorageServiceProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/load-data', {
+                templateUrl: 'views/load-data.html',
+                controller: 'LoadDataCtrl',
+                controllerAs: 'ctrl'
+            })
+            .when('/review-fields/:csvCacheId', {
+                templateUrl: 'views/reviewfields.html',
+                controller: 'ReviewfieldsCtrl',
+                controllerAs: 'ctrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+
+        // Configure LocalStorage
+        localStorageServiceProvider.setPrefix('foundersMapQuest');
+        localStorageServiceProvider.setStorageType('localStorage');
+    })
+    .run(function($rootScope, $route) {
+        $rootScope.$route = $route;
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,10 @@
  * Controller of the foundersMapQuestApp
  */
 angular.module('foundersMapQuestApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
